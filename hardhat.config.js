757b5ab4de7c68f38cf88b93026b3516932fa6bc
jkaspereit <j.kaspereit1994@gmail.com>
2022-07-14 11:36:22 +0200

    reset config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,4 @@
 require("@nomiclabs/hardhat-waffle");
-const secret = require("./environment/secrets.json")
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -22,16 +21,11 @@ module.exports = {
     cache: "./blockchain/cache",
     artifacts: "./blockchain/artifacts"
   },
-  defaultNetwork: "testnet",
+  defaultNetwork: "ropsten",
   networks: {
-    hardhat: {
-      forking: {
-        url: "https://ropsten.infura.io/v3/db8c2281c9384ffa918e3441743c8d58"
-      }
-    },
-    testnet: {
-      url: "https://ropsten.infura.io/v3/db8c2281c9384ffa918e3441743c8d58",
-      accounts: [secret.pirvatekey]
+    ropsten: {
+      url: "",
+      accounts: [""]
     }
   }
 };
